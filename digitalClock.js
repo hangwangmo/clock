2b4bc315c52b044bb604b0f1edadcb4427eafdcd
hangwangmo <95513594+hangwangmo@users.noreply.github.com>
2022-06-12 15:11:45 +0900

    Add files via upload

---
+++ digitalClock.js
@@ -0,0 +1,42 @@
+const timeDiv = document.getElementById("clock");
+const dateDiv = document.getElementById("date");
+
+function getCalendarTime() {
+
+    const dayList = ["일","월","화","수","목","금","토"];
+
+    let now = new Date();
+
+    let year = now.getFullYear();
+    let month = now.getMonth() + 1;
+    let day = now.getDate();
+    let dayNumber = now.getDay();
+
+    let hour = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
+    let noon = "AM";
+
+    if(hour > 11) {
+        noon = "PM"
+        hour = hour - 12;
+        if(hour == 0) {
+            hour = 12;
+        }
+    }
+
+    dateDiv.textContent = year + "-" + formatNumber(month) + "-" + formatNumber(day) + " " + dayList[dayNumber];
+    timeDiv.textContent = noon + " " + formatNumber(hour) + ":" + formatNumber(minutes) + ":" + formatNumber(seconds);
+}
+
+function formatNumber(number) {
+    if(number<10) {
+        return "0" + number;
+    }
+
+    return number;
+}
+
+setInterval(function() {
+    getCalendarTime();
+}, 1000);
